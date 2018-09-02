80e0b9d9d9e08f6ff094737d05b3b2c17c18f666
Peter Crighton <git@petercrighton.de>
2018-09-02 21:44:52 +0200

    Adjust title

--- www/site/snippets/header.php
+++ www/site/snippets/header.php
@@ -2,9 +2,16 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <?php echo liveCSS('assets/builds/bundle.css') ?>
+  <title>
+    <?php if(!$page->isHomePage()): ?>
+      <?= $page->title()->html() ?> |
+    <?php endif ?>
+    <?= $site->title()->html() ?>
+  </title>
+  
+  <?= liveCSS('assets/builds/bundle.css') ?>
 </head>
+  
 <body>
